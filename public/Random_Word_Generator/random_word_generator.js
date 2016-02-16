e92b55b5826281ed6a3d72d4ad151ae79bdf601b
Nicholas Vrzalik <Nicholas@Nicholass-MacBook-Pro-2.local>
2016-02-16 16:08:29 -0600

    Add "ajax_store.html"

---
+++ public/Random_Word_Generator/random_word_generator.js
@@ -0,0 +1,6 @@
+// var wordArray = [];
+// 		wordArray = $('#hidden').text().split('\n');
+
+// 			function generateRandomWord() {
+// 				return wordArray[Math.floor(Math.random() * wordArray.length)];
+// 			}
