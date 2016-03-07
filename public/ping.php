e787a1b71e2efe5540de487598afe7efdc0d9ee8
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-07 15:42:14 -0600

    Created and finished ping.php and pong.php using counter.php

---
+++ public/ping.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+function pageController() {
+
+	if (isset($_GET['counter'])) {
+		$counter = $_GET['counter'];
+	} elseif (isset($_GET['miss']) && $_GET['miss'] == true) {
+		$counter = 0;
+	} else {
+		$counter = 0;
+	}
+	$data = [];
+	$data['counter'] = $counter;
+
+	return $data;
+}
+
+extract(pageController());
+
+
+
+ ?>
+
+
+ <!doctype html>
+
+ <html>
+ <head>
+ 	<title>Ping</title>
+ </head>
+ <body>
+ 	<h1><?php echo $counter ?></h1>
+ 	<a href="http://codeup.dev/pong.php?counter=<?php echo $counter + 1 ?>&miss=<?php echo false ?>">Hit</a>
+ 	<a href="http://codeup.dev/pong.php?miss=<?php echo true ?>">Miss</a>
+ </body>
+ </html>
