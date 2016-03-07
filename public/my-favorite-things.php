ca6d174dae30afebcfd7ce0ca112afac214d9ea8
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-07 11:20:04 -0600

    Edit my-favorite-things.php to use alternative syntax

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -3,7 +3,21 @@
 
 $array = ['Stuff', 'Things', 'Other things', 'Running out of words', 'Thingies'];
 
-
-
-
  ?>
+
+
+ <!doctype html>
+
+ <html>
+	 <head>
+	 	<title>Favorite Things</title>
+	 </head>
+	 <body>
+	 	<table>
+
+	 		<?php foreach ($array as $key => $value): ?>
+	 			<tr><td><?= $value; ?></td></tr>
+	 		<?php endforeach; ?>
+	 	</table>
+	 </body>
+ </html>
