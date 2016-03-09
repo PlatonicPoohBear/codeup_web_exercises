2608e7bb7f34325c47b8bc477361d8167fcd3b13
PlatonicPoohBear <biologynut10@hotmail.com>
2016-03-09 09:34:08 -0600

    Created a template site using "include"

---
+++ public/index_test.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+ <html>
+ <head>
+ 	<title>Template Site</title>
+ </head>
+ <body>
+ 	
+ 	<?php include_once 'header.html'; ?>
+
+ 	<?php include_once 'navbar.html'; ?>
+
+ 	<?php include_once 'footer.html'; ?>
+ 	
+ </body>
+ </html>
